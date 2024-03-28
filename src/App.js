ddaa87b6bd19da82c36939c44686bdc46eb63051
Amarildo de Arruda <amaral77@gmail.com>
2024-03-28 17:26:32 -0400

    Formulario

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 
 import Banner from './componentes/Banner/Banner';
 import CampoTexto from './componentes/CampoTexto';
+import Formulario from './componentes/Formulario';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      {/*  Foi removido daqui e jogado para dentro do js do Formulario
       <CampoTexto label='Nome' placeholder="Digite o nome"/>
       <CampoTexto label='Cargo' placeholder="Digite o cargo"/>
       <CampoTexto label='Imagem' placeholder="Selecione a imagem"/>
-      
+      */}
+      <Formulario />
     </div>
   );
 }
