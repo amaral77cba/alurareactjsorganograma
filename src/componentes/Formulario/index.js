ed075bd3ac5563bfa2a0feeb35f09775b331b303
Amarildo de Arruda <amaral77@gmail.com>
2024-04-03 22:21:16 -0400

    Alteracoes 03042024

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -5,7 +5,7 @@ const Formulario = () => {
     return (
         <section className='formulario'>
             <form>
-                <h2>Preencha os dados para criar o card</h2>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
                 <CampoTexto label='Nome' placeholder="Digite o nome" />
                 <CampoTexto label='Cargo' placeholder="Digite o cargo" />
                 <CampoTexto label='Imagem' placeholder="Selecione a imagem" />
